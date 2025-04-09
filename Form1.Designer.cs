4c863066f3d535868a9771854777d1a1ac82573f
kimjihye058 <s2321@e-mirim.hs.kr>
2025-04-09 10:23:18 +0900

    GUI 연습; 버튼, label 추가하기

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HellowWorldWindowFrom
 {
-    partial class Form1
+    partial class form1
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(341, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(303, 71);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "누르지마시오";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(334, 118);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(310, 40);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "안녕하세요 ㅎㅎ";
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(23F, 40F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1015, 470);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("굴림", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Margin = new System.Windows.Forms.Padding(10);
+            this.Name = "form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "3103김지혜";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTitle;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace HellowWorldWindowFrom
 {
-    public partial class Form1: Form
+    public partial class form1: Form
     {
-        public Form1()
+        public form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblTitle.Text = "탈락입니다.";
         }
     }
 }
