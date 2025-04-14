ab6fa39ea63f656af1eb21dbac6a42cbf8baea42
kimjihye058 <s2321@e-mirim.hs.kr>
2025-04-14 15:11:16 +0900

    메모장 파일 저장 구현

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -164,6 +164,7 @@
             this.열기ToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
             this.열기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.열기ToolStripMenuItem.Text = "열기";
+            this.열기ToolStripMenuItem.Click += new System.EventHandler(this.열기ToolStripMenuItem_Click);
             // 
             // 저장ToolStripMenuItem
             // 
--- FormMain.cs
+++ FormMain.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -39,6 +40,25 @@ namespace HellowWorldWindowFrom
             formAbout2.Show();
         }
 
-        
+        private void 열기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "텍스트파일(*.txt)|*.txt";   // 파일 형식을 지정해줄 수 있음
+            DialogResult result = openFileDialog.ShowDialog();
+            switch (result) {
+                case DialogResult.OK:
+                    // textBox1.Text = openFileDialog.FileName;    // 파일 경로 나옴
+                    var fileStream = new FileStream(openFileDialog.FileName, FileMode.Open);
+                    using(StreamReader reader = new StreamReader(fileStream))
+                    {
+                        textBox1.Text = reader.ReadToEnd();
+                    }
+                    break;
+                case DialogResult.Cancel:
+                    MessageBox.Show("취소했습니다.");
+                    break;
+            }
+
+        }
     }
 }
